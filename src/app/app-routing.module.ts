da121385fbd302a16e4f3cf6a6a8a65dec752354
codeslinger35 <goaliebuddy@gmail.com>
2023-07-22 02:47:21 +0000

    refactor: reorganize the home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ResumeComponent } from './resume/resume.component';
+import { DogComponent } from './dog/dog.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'resume', component: ResumeComponent }
+  { path: 'resume', component: ResumeComponent },
+  { path: 'dog', component: DogComponent }
 ];
 
 @NgModule({
